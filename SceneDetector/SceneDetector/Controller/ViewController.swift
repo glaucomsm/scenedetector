3b1937ad7b3f7e473f6324ad9c6e593b136633ac
Glauco Moraes <glaucomsm@gmail.com>
2017-10-03 19:49:41 -0300

    Implement CoreML

--- SceneDetector/SceneDetector/Controller/ViewController.swift
+++ SceneDetector/SceneDetector/Controller/ViewController.swift
@@ -7,29 +7,101 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var scene: UIImageView!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    // MARK: - Properties
+    let vowels: [Character] = ["a", "e", "i", "o", "u"]
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        guard let image = UIImage(named: "dark_road") else {
+            fatalError("no starting image")
+        }
+        
+        scene.image = image
+        
+        guard let ciImage = CIImage(image: image) else {
+            fatalError("couldn't convert UIImage to CIImage")
+        }
+        
+        detectScene(image: ciImage)
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+// MARK: - IBActions
+extension ViewController {
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func pickImage(_ sender: Any) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .savedPhotosAlbum
+        present(pickerController, animated: true)
     }
-    */
+}
 
+// MARK: - Methods
+extension ViewController {
+    
+    func detectScene(image: CIImage) {
+        answerLabel.text = "detecting scene..."
+        guard let model = try? VNCoreMLModel(for: GoogLeNetPlaces().model) else {
+            fatalError("can't load Places ML model")
+        }
+        
+        // Create a Vision request with completion handler
+        let request = VNCoreMLRequest(model: model) { [weak self] request, error in
+            guard let results = request.results as? [VNClassificationObservation],
+                let topResult = results.first else {
+                    fatalError("unexpected result type from VNCoreMLRequest")
+            }
+            
+            // Update UI on main queue
+            let article = (self?.vowels.contains(topResult.identifier.first!))! ? "an" : "a"
+            DispatchQueue.main.async { [weak self] in
+                self?.answerLabel.text = "\(Int(topResult.confidence * 100))% it's \(article) \(topResult.identifier)"
+            }
+        }
+        
+        // Run the Core ML GoogLeNetPlaces classifier on global dispatch queue
+        let handler = VNImageRequestHandler(ciImage: image)
+        DispatchQueue.global(qos: .userInteractive).async {
+            do {
+                try handler.perform([request])
+            } catch {
+                print(error)
+            }
+        }
+    }
+}
+
+// MARK: - UIImagePickerControllerDelegate
+extension ViewController: UIImagePickerControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        dismiss(animated: true)
+        guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("couldn't load image from Photos")
+        }
+        
+        scene.image = image
+        
+        guard let ciImage = CIImage(image: image) else {
+            fatalError("couldn't convert UIImage to CIImage")
+        }
+        
+        detectScene(image: ciImage)
+    }
+}
+
+// MARK: - UINavigationControllerDelegate
+extension ViewController: UINavigationControllerDelegate {
 }
